ecd0bec6949b102f0f6bd13be9cbd3ca8608164e
woj141 <32813454+woj141@users.noreply.github.com>
2019-03-02 18:16:38 +0100

    Add code that allows the content of a file to be read

--- readingFile.java
+++ readingFile.java
@@ -1,8 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class readingFile {
     public static void main(String[] args) {
         Path path = Paths.get("C:\\test\\testFile\\testFile.txt");
+        try {
+            for (String line : Files.readAllLines(path)) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
