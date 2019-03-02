a53bf1518af384d5c4b13ab573744e99b96d35d8
woj141 <32813454+woj141@users.noreply.github.com>
2019-03-02 18:10:13 +0100

    Add test path

--- readingFile.java
+++ readingFile.java
@@ -1,5 +1,8 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class readingFile {
     public static void main(String[] args) {
-
+        Path path = Paths.get("C:\\test\\testFile\\testFile.txt");
     }
 }
